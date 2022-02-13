dec47fa7bc65ae83ae04ec2f7c5c5912bbeb11a8
Eugene <evgen29031999@bk.ru>
2022-02-13 23:15:07 +0300

    add APIManager and model

---
+++ App/MyLocation/Model/APIManager.swift
@@ -0,0 +1,88 @@
+//
+//  APIManager.swift
+//  WeatherApp
+//
+//  Created by Eugene on 13.02.2022.
+//
+
+import Foundation
+
+enum APIResult<LocationModel> {
+    case Success(LocationModel)
+    case Failure(Error)
+}
+
+protocol APIManager {
+    var sessionConfiguration: URLSessionConfiguration { get }
+    var session: URLSession { get }
+    
+    //получает данные
+    func JSONTaskWith(request: URLRequest, completionHandler: @escaping (LocationModel?, HTTPURLResponse?, Error?) -> Void) -> URLSessionDataTask
+    
+    //использует данные для обновленя интерфейса
+    func fetch<LocationModel>(request: URLRequest, completionHandler: @escaping (APIResult<LocationModel>) -> Void)
+    
+    
+}
+
+extension APIManager {
+    func JSONTaskWith(request: URLRequest, completionHandler: @escaping (LocationModel?, HTTPURLResponse?, Error?) -> Void) -> URLSessionDataTask{
+        
+        let decoder = JSONDecoder()
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            
+            //проверяем наличие HTTPURLResponse
+            guard let HTTPResponse = response as? HTTPURLResponse else {
+                
+                let userInfo = [NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")]
+                let error = NSError(domain: NetworkingErrorDomain, code: MissingHTTPResponseError, userInfo: userInfo)
+                
+                //если нет, то возвращаем в completionHandler данные
+                completionHandler(nil, nil, error)
+                return
+            }
+            
+            //проверяем пустые ли данные
+            if data == nil{
+                if let error = error{
+                    
+                    //если да и есть ошибка, то возвращаем в completionHandler данные
+                    completionHandler(nil, HTTPResponse, error)
+                }
+            }else{
+                switch HTTPResponse.statusCode{
+                case 200:
+                    do{
+                        //сериализуем данные из json формата в объект Foundation
+                        let weatherData = try decoder.decode(LocationModel.self, from: data!)
+                        completionHandler(weatherData, HTTPResponse, nil)
+                    }catch let error as NSError{
+                        completionHandler(nil, HTTPResponse, error)
+                    }
+                default:
+                    print("We have got response status \(HTTPResponse.statusCode)")
+                }
+            }
+        }
+        return dataTask
+    }
+    
+    func fetch<LocationModel>(request: URLRequest, completionHandler: @escaping (APIResult<LocationModel>) -> Void){
+        
+        let dataTask = JSONTaskWith(request: request) { weatherData, response, error in
+            guard let weatherData = weatherData else{
+                if let error = error {
+                    completionHandler(.Failure(error))
+                }
+                return
+            }
+            
+            if let weatherData = weatherData as? LocationModel {
+                completionHandler(.Success(weatherData))
+            }
+        }
+        
+        dataTask.resume()
+    }
+}
---
+++ App/MyLocation/Model/LocationModel.swift
@@ -0,0 +1,44 @@
+//
+//  LocationModel.swift
+//  WeatherApp
+//
+//  Created by Eugene on 13.02.2022.
+//
+
+import Foundation
+import UIKit
+
+struct LocationModel: Codable {
+    var latitude: Double
+    var longtitude: Double
+    var location: String
+    var current: Current
+    
+    enum CodingKeys: String, CodingKey {
+        case latitude = "lat"
+        case longtitude = "lon"
+        case location = "timezone"
+        case current
+    }
+}
+
+struct Current: Codable {
+    var temperature: Double
+    var feeling: Double
+    var wind: Double
+    var weather: [Weather]
+    
+    enum CodingKeys: String, CodingKey {
+        case temperature = "temp"
+        case feeling = "feels_like"
+        case wind = "wind_speed"
+        case weather
+        
+    }
+}
+
+struct Weather: Codable {
+    var main: String
+    var description: String
+    var icon: String
+}
