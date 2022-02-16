7a44b58af77cba8929762987cdf72e5b69884985
Eugene <evgen29031999@bk.ru>
2022-02-16 23:52:55 +0300

    add initializer

--- App/General/Utility/WeatherIconManager.swift
+++ App/General/Utility/WeatherIconManager.swift
@@ -19,4 +19,19 @@ enum WeatherIconManager: String {
     case Snow = "13d"
     case Mist = "50d"
     
+    init(rawValue: String) {
+        switch rawValue {
+        case "01d": self = .ClearSky
+        case "02d": self = .FewClouds
+        case "03d": self = .ScatteredClouds
+        case "04d": self = .BrokenClouds
+        case "09d": self = .ShowerRain
+        case "10d": self = .Rain
+        case "11d": self = .ThunderStorm
+        case "13d": self = .Snow
+        case "50d": self = .Mist
+        default:
+            self = .ClearSky
+        }
+    }
 }
